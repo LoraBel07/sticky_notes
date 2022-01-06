fee1ec1f6a7c0a2495165d9dd3f180c51ca2909d
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-01-05 23:32:03 -0500

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let container2 = document.getElementsByClassName("container2")[0];
+let container3 = document.getElementsByClassName("container3")[0];
+let checkIcon = document.getElementById("check-icon");
+let xIcon = document.getElementById("x-icon");
+let i=0;
+
+xIcon.addEventListener("click", function())
